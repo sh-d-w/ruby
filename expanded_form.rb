4baa2631fe44dd8a0f9fc175508b90c889733152
sh_d_w <sharayzah321@gmail.com>
2021-05-29 22:05:18 +0200

    concatenate in string output

--- expanded_form.rb
+++ expanded_form.rb
@@ -1,5 +1,3 @@
-# Hello World Program in Ruby
-
 # 1. define function and return string
 # 2. loop through number % 10
 # 3. add conditions if number not % 10
@@ -12,8 +10,11 @@ def expanded_form(p_number)
     t_number = 1
     while number > 0
         if number % 10 != 0
-            puts (number % 10) * t_number
-
+            if str == ""
+                str = ((number % 10) * t_number).to_s
+            else
+                str = ((number % 10) * t_number).to_s + " + " + str
+            end
             number -= (number % 10)
         else
             t_number *= 10
@@ -24,7 +25,9 @@ def expanded_form(p_number)
 return str
 end
 
-puts "Hello World!" + expanded_form(80504);
+puts expanded_form(12);
+puts expanded_form(42);
+puts expanded_form(80504);
 
 # expanded_form(12); // Should return '10 + 2'
 # expanded_form(42); // Should return '40 + 2'
