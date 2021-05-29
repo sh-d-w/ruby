8a5b13e0119af935c6b45ea976516923d1cfcc52
sh_d_w <sharayzah321@gmail.com>
2021-05-29 21:55:33 +0200

    define function

---
+++ expanded_form.rb
@@ -0,0 +1,19 @@
+# Hello World Program in Ruby
+
+# 1. define function and return string
+# 2. loop through number % 10
+# 3. add conditions if number not % 10
+# 4. add iterator for nth value
+# 5. concatenate in string output
+
+def expanded_form(p_number)
+    str = "test"
+
+return str
+end
+
+puts "Hello World!" + expanded_form(12);
+
+# expanded_form(12); // Should return '10 + 2'
+# expanded_form(42); // Should return '40 + 2'
+# expanded_form(80504); // Should return '80000 + 500 + 4'
